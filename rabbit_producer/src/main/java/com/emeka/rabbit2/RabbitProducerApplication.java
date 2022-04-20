263fd6d2b9803641294b6f5ff331b4820a818f40
chukwudozie <emkychuks062@gmail.com>
2022-04-20 14:46:14 +0100

    modify the main class and update application.yml

--- rabbit_producer/src/main/java/com/emeka/rabbit2/RabbitProducerApplication.java
+++ rabbit_producer/src/main/java/com/emeka/rabbit2/RabbitProducerApplication.java
@@ -4,9 +4,7 @@ import com.emeka.rabbit2.entity.DummyMessage;
 import com.emeka.rabbit2.entity.InvoiceCancelledMessage;
 import com.emeka.rabbit2.entity.InvoiceCreatedMessage;
 import com.emeka.rabbit2.entity.InvoicePaidMessage;
-import com.emeka.rabbit2.producer.DummyProducer;
-import com.emeka.rabbit2.producer.InvoiceProducer;
-import com.emeka.rabbit2.producer.MultiplePrefetchProducer;
+import com.emeka.rabbit2.producer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,32 +23,54 @@ public class RabbitProducerApplication  implements CommandLineRunner {
     }
     @Autowired
 //    private MultiplePrefetchProducer producer;
-    private InvoiceProducer producer;
+//    private InvoiceProducer producer;
+//    private SingleActiveProducer producer;
+    private ReliableProducer producer;
 
+//    @Override
+//    public void run(String... args) throws Exception {
+////        for(int i = 0; i < 500; i++) {
+////            DummyMessage message = new DummyMessage("Now is " + LocalTime.now(), 1);
+////            producer.sendDummy(message);
+////            TimeUnit.SECONDS.sleep(1);
+////        producer.simulateTransaction();
+////        producer.simulateScheduler();
+////        }
+//       String randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(100,200);
+//
+//        InvoiceCreatedMessage invoiceCreatedMessage =
+//         new InvoiceCreatedMessage(150.0, LocalDate.now().minusDays(2),"USD",randomInvoiceNumber);
+//        producer.sendInvoiceCreated(invoiceCreatedMessage);
+//        randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(200,300);
+//        String randomPaymentNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(800,1000);
+//        InvoicePaidMessage invoicePaidMessage =
+//                new InvoicePaidMessage(randomInvoiceNumber,LocalDate.now(),randomPaymentNumber);
+//        producer.sendInvoicePaid(invoicePaidMessage);
+//
+//        randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(300,400);
+//        InvoiceCancelledMessage invoiceCancelledMessage =
+//                new InvoiceCancelledMessage(LocalDate.now(),randomInvoiceNumber, "testing ...");
+//        producer.sendInvoiceCancelled(invoiceCancelledMessage);
+//        System.out.println("All sent");
+//    }
+
+    // for demonstrating consistent hash exchange
     @Override
     public void run(String... args) throws Exception {
-//        for(int i = 0; i < 500; i++) {
-//            DummyMessage message = new DummyMessage("Now is " + LocalTime.now(), 1);
-//            producer.sendDummy(message);
-//            TimeUnit.SECONDS.sleep(1);
-//        producer.simulateTransaction();
-//        producer.simulateScheduler();
+//        for (int i = 0; i < 200; i++){
+//            String invoiceNumber = "INV-"+ (i % 60);
+//            InvoiceCreatedMessage invoice = new InvoiceCreatedMessage(ThreadLocalRandom.current().nextInt(200),
+//                    LocalDate.now(), "USD",invoiceNumber);
+//            producer.sendInvoiceCreated(invoice);
 //        }
-       String randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(100,200);
+//        producer.sendDummy(); // for SAC
 
-        InvoiceCreatedMessage invoiceCreatedMessage =
-         new InvoiceCreatedMessage(150.0, LocalDate.now().minusDays(2),"USD",randomInvoiceNumber);
-        producer.sendInvoiceCreated(invoiceCreatedMessage);
-        randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(200,300);
-        String randomPaymentNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(800,1000);
-        InvoicePaidMessage invoicePaidMessage =
-                new InvoicePaidMessage(randomInvoiceNumber,LocalDate.now(),randomPaymentNumber);
-        producer.sendInvoicePaid(invoicePaidMessage);
-
-        randomInvoiceNumber =  "INV-"+ ThreadLocalRandom.current().nextInt(300,400);
-        InvoiceCancelledMessage invoiceCancelledMessage =
-                new InvoiceCancelledMessage(LocalDate.now(),randomInvoiceNumber, "testing ...");
-        producer.sendInvoiceCancelled(invoiceCancelledMessage);
-        System.out.println("All sent");
+        DummyMessage dummy1 = new DummyMessage("Invalid key",10);
+        DummyMessage dummy2 = new DummyMessage("Invalid exchange", 20);
+        producer.sendDummyWithInvalidRoutingKey(dummy1);
+        producer.sendDummyWithInvalidExchange(dummy2);
+        System.out.println("done");
     }
+
+
 }
